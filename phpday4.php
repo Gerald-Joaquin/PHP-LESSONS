212c5fc9780f07b60b21a79b1157b8d46c99c774
Gerald Joaquin <116679273+Gerald-Joaquin@users.noreply.github.com>
2025-04-02 21:04:29 +0800

    Add files via upload

---
+++ phpday4.php
@@ -0,0 +1,31 @@
+<!-- CONDITIONAL STATEMENTS -->
+
+<?php
+
+// IF, ELSEIF, ELSE STATEMENTS
+    $age = 17;
+
+    if ($age >= 60) {
+        echo"You are an Senior Citizen";
+    } elseif ($age >= 18) {
+        echo"You are an adult!";  
+    } else {
+        echo"You are not an adult!";
+    }
+// ---------------------------------------
+    $gwa = 75;
+
+    if ($gwa == 100) {
+        echo"EXCELLENT!";
+    } elseif ($gwa >= 95) {
+        echo"VERYGOOD!";
+    } elseif ($gwa >= 90) {
+        echo"GOOD!";
+    } elseif ($gwa >= 80) {
+        echo"IT'S ALRIGHT!";
+    } elseif ($gwa >= 75) {
+        echo"DAMNNNNNN!";
+    } else {
+        echo"MISS U!";
+    }
+?>
